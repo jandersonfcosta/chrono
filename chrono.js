3caffa218ae6800f19b1586384a48019a778a04f
jandersonfcosta <jandersonfcosta@gmail.com>
2016-10-10 22:59:40 -0300

    1001110011101

--- chrono.js
+++ chrono.js
@@ -2,56 +2,57 @@
     Chrono
     Descrição: Cronômetro com métodos start, stop, reset e getTime.
     Criado por Janderson costa em 10/10/2016.
+
     Uso: ver demo.html
 */
 
 function Chrono() {
-    var _this = this,
-        H = 0,
-        M = 0,
-        S = 0,
-        interval;
+	var _this = this,
+		H = 0,
+		M = 0,
+		S = 0,
+		interval;
 
-    this.isStoped = true;
-    this.isRunning = false;
+	this.isStoped = true;
+	this.isRunning = false;
 
-    this.start = function(time, callback) {
-        // time: "hh:mm:ss"
+	this.start = function(time, callback) {
+		// time: "hh:mm:ss"
 
-        _this.isRunning = true;
-        _this.isStoped = false;
+		_this.isRunning = true;
+		_this.isStoped = false;
 
-        if (time) {
+		if (time) {
 			time = time.split(":");
 			H = Number(time[0] || 0);
 			M = Number(time[1] || 0);
 			S = Number(time[2] || 0);
 		}
 
-        if (interval)
-            clearInterval(interval);
+		if (interval)
+			clearInterval(interval);
 
-        interval = setInterval(function() {
+		interval = setInterval(function() {
 			setS();
 
-            if (callback)
-                callback(formatTime());
+			if (callback)
+				callback(formatTime());
 		}, 1000);
-    };
+	};
 
-    this.stop = function() {
-        _this.isStoped = true;
-        _this.isRunning = false;
-        clearInterval(interval);
-    };
+	this.stop = function() {
+		_this.isStoped = true;
+		_this.isRunning = false;
+		clearInterval(interval);
+	};
 
-    this.reset = function() {
-        H = 0;
-        M = 0;
-        S = 0;
-    };
+	this.reset = function() {
+		H = 0;
+		M = 0;
+		S = 0;
+	};
 
-    this.getTime = function() {
+	this.getTime = function() {
 		return formatTime();
 	};
 
@@ -77,11 +78,11 @@ function Chrono() {
 		H += 1;
 	}
 
-    function formatTime() {
-        var h = H < 10 ? "0" + H : H,
-            m = M < 10 ? "0" + M : M,
-            s = S < 10 ? "0" + S : S;
+	function formatTime() {
+		var h = H < 10 ? "0" + H : H,
+			m = M < 10 ? "0" + M : M,
+			s = S < 10 ? "0" + S : S;
 
-        return h + ":" + m + ":" + s;
-    }
+		return h + ":" + m + ":" + s;
+	}
 }
